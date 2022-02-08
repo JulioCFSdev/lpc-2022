bd4b2223d9e3fd2fb21d6f3335b8b49925a51637
JulioCFSdev <90019843+JulioCFSdev@users.noreply.github.com>
2022-02-07 21:32:02 -0300

    consertando o pong

--- my_pong.py
+++ my_pong.py
@@ -1,10 +1,9 @@
 import turtle
-import os
 
 screen = turtle.Screen()
 screen.title("My Pong")
 screen.bgcolor("black")
-screen.seput(width=800, height=600)
+screen.setup(width=800, height=600)
 screen.tracer(0)
 
 # draw paddle 1
@@ -12,19 +11,20 @@ paddle_1 = turtle.Turtle()
 paddle_1.speed(0)
 paddle_1.shape("square")
 paddle_1.color("white")
-paddle_1.shapesize(stretch_wid=5, strentch_len=1)
+paddle_1.shapesize(stretch_wid=5, stretch_len=1)
 paddle_1.penup()
 paddle_1.goto(-350, 0)
 
 # draw paddle 2
-paddle_1 = turtle.Turtle()
-paddle_1.speed(0)
-paddle_1.shape("square")
-paddle_1.color("white")
-paddle_1.shapesize(stretch_wid=5, strentch_len=1)
-paddle_1.penup()
-paddle_1.goto(350, 0)
+paddle_2 = turtle.Turtle()
+paddle_2.speed(0)
+paddle_2.shape("square")
+paddle_2.color("white")
+paddle_2.shapesize(stretch_wid=5, stretch_len=1)
+paddle_2.penup()
+paddle_2.goto(350, 0)
 
+# draw ball
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape("square")
@@ -34,6 +34,7 @@ ball.goto(0, 0)
 ball.dx = 1
 ball.dy = 1
 
+# draw screen
 hud = turtle.Turtle()
 hud.speed(0)
 hud.shape("square")
@@ -41,10 +42,54 @@ hud.color("white")
 hud.penup()
 hud.hideturtle()
 hud.goto(0, 260)
-hud.write("0 : 0", align="center", font=("Press Start 2P", 24, "normal"))
+hud.write("0 : 0", align="center", font=("Small fonts", 24, "normal"))
+
+
+def paddle_1_up():
+
+    y = paddle_1.ycor()
+    if y > 250:
+        y += 30
+    else:
+        y = 250
+    paddle_1.sety(y)
+
+
+def paddle_1_down():
+    y = paddle_1.ycor()
+    if y > -250:
+        y += -30
+    else:
+        y = -250
+    paddle_1.sety(y)
+
+
+def paddle_2_up():
+
+    y = paddle_2.ycor()
+    if y > 250:
+        y += 30
+    else:
+        y = 250
+    paddle_1.sety(y)
+
+
+def paddle_2_down():
+
+    y = paddle_2.ycor()
+    if y > -250:
+        y += -30
+    else:
+        y = -250
+    paddle_2.sety(y)
+
+
+screen.listen()
+screen.onkeypress(paddle_1_up, "w")
+screen.onkeypress(paddle_1_down, "s")
+screen.onkeypress(paddle_2_up, "up")
+screen.onkeypress(paddle_2_down, "down")
 
 # game loop
 while True:
     screen.update()
-
-screen.exitonclick()
